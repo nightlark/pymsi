7de214c8c31c071b5548f72bfbd45811a7f8c851
Asriel Margarian <margarian1@llnl.gov>
2025-05-29 13:55:57 -0700

    Add .msi-specific parsing

---
+++ src/pymsi/msi/file.py
@@ -0,0 +1,36 @@
+from typing import Dict, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .component import Component
+    from .media import Media
+
+
+# https://learn.microsoft.com/en-us/windows/win32/msi/file-table
+class File:
+    def __init__(self, row: Dict):
+        self.id: str = row["File"]
+        self._component: str = row["Component_"]
+        self.name: str = row["FileName"]
+        self.size: int = row["FileSize"]
+        self.version = row["Version"]
+        langs = row["Language"]
+        langs = langs.split(",") if isinstance(langs, str) else []
+        self.language = [lang.strip() for lang in langs if lang.strip()]
+        self.attributes: int = row["Attributes"]
+        self.sequence: int = row["Sequence"]
+        
+    def _populate(self, component_map: Dict[str, "Component"], media_map: Dict[int, "Media"]):
+        self.component = component_map[self._component]
+        self.component._add_file(self)
+        
+        self.media = min([media for media in media_map.values() if media.last_sequence >= self.sequence], key=lambda m: m.last_sequence)
+        
+    def pretty_print(self, indent: int = 0):
+        print(" " * indent + f"File: {self.name} ({self.id})")
+        print(" " * (indent + 2) + f"Size: {self.size} bytes")
+        print(" " * (indent + 2) + f"Version: {self.version}")
+        print(" " * (indent + 2) + f"Language(s): {', '.join(self.language)}")
+        print(" " * (indent + 2) + f"Attributes: {self.attributes}")
+        print(" " * (indent + 2) + f"Sequence: {self.sequence}")
+        print(" " * (indent + 2) + f"Component: {self.component.id} ({self.component.directory.name})")
+        print(" " * (indent + 2) + f"Media: {self.media.id} (Last Sequence: {self.media.last_sequence})")
---
+++ src/pymsi/msi/media.py
@@ -0,0 +1,24 @@
+from typing import Dict, Optional
+
+
+# https://learn.microsoft.com/en-us/windows/win32/msi/media-table
+class Media:
+    def __init__(self, row: Dict):
+        self.id: int = row["DiskId"]
+        self.last_sequence: int = row["LastSequence"]
+        self.disk_prompt: Optional[str] = row["DiskPrompt"]
+        self.cabinet: Optional[str] = row["Cabinet"]
+        self.volume_label: Optional[str] = row["VolumeLabel"]
+        self.source: Optional[str] = row["Source"]
+        
+    def pretty_print(self, indent: int = 0):
+        print(" " * indent + f"Media: {self.id}")
+        print(" " * (indent + 2) + f"Last Sequence: {self.last_sequence}")
+        if self.disk_prompt:
+            print(" " * (indent + 2) + f"Disk Prompt: {self.disk_prompt}")
+        if self.cabinet:
+            print(" " * (indent + 2) + f"Cabinet: {self.cabinet}")
+        if self.volume_label:
+            print(" " * (indent + 2) + f"Volume Label: {self.volume_label}")
+        if self.source:
+            print(" " * (indent + 2) + f"Source: {self.source}")
