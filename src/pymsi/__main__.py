29d5147a43f220d69c0b1d7807ad43173d6a1162
Asriel Margarian <margarian1@llnl.gov>
2025-05-30 09:54:57 -0700

    Fix formatting

--- src/pymsi/__main__.py
+++ src/pymsi/__main__.py
@@ -1,3 +1,9 @@
+# Based on MSI file format documentation/code from:
+# https://github.com/GNOME/msitools/blob/4343c982665c8b2ae8c6791ade9f93fe92caf79c/libmsi/table.c
+# https://github.com/mdsteele/rust-msi/blob/master/src/internal/streamname.rs
+# https://stackoverflow.com/questions/9734978/view-msi-strings-in-binary
+
+
 from pathlib import Path
 
 if __name__ == "__main__":
